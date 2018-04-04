c1bcd99cc7f23f0fcd379a8ca2c2c8050c675991
chengweidong <chengweidong@smartisan.com>
2018-04-04 16:44:24 +0800

    edit update

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd /data/www/xs/$2
-newest=`ls | egrep "^[0-9]" | sort | tail -1`
+newest=`ls | egrep "^[0-9]" | sort -n | tail -1`
 cd /data/www/xs/
 php UpdateCommand.php $1 $2 $3 $newest
